f5141bead3808608b83badec184c19c37d2d7274
danielshapira1 <92857488+danielshapira1@users.noreply.github.com>
2022-10-16 15:43:43 +0300

    v1.3 added 5 screens added firebase authentication added calender & graphs

--- app/src/main/java/com/example/myfinance/SignupActivity.java
+++ app/src/main/java/com/example/myfinance/SignupActivity.java
@@ -4,14 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+
 import android.widget.Toast;
 
 import com.example.myfinance.databinding.ActivitySignupBinding;
